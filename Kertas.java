8951ec9212f43097671bad1be9d650594bc1af42
victoria-angelique <victoriaangelique01@gmail.com>
2023-04-23 22:45:08 +0700

    add method menulis

--- Kertas.java
+++ Kertas.java
@@ -1,8 +1,19 @@
+import java.util.*; 
+
 public class Kertas extends NonMakanan {
     public Kertas(){
         super(1, 1, 1);
     }
     public void menulis(){
         //implementation code goes here
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Masukkan kata atau kalimat yang ingin ditulis:");
+        // String input
+        String tulisan = scan.nextLine();
+        // Output input by user
+        System.out.println(tulisan);
+
+        scan.close();
     }
 }
