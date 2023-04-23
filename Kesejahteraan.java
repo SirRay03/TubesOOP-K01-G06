02cbce928da58fa7d8519e7ea102ef4d822955e9
WillyFFS <willyfrans13@gmail.com>
2023-04-23 21:56:03 +0700

    fix typo

--- Kesejahteraan.java
+++ Kesejahteraan.java
@@ -85,7 +85,7 @@ public class Kesejahteraan{
 
     public void olahRaga (Time time){
         long timeLong = time.getTime()/ 1000; 
-        long pengali = Math.floorDiv(timeLong, timeLong);
+        long pengali = Math.floorDiv(timeLong, 20);
         int pengaliInt = ((int)pengali);
         setMood(pengaliInt * 10);
         setHealth(pengaliInt * -5);
